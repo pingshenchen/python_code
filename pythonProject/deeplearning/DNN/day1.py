88e5f0d7fd54bf517fe0dc425fc61d36df51bbf4
pingshenchen <cyt370746@gmail.com>
2025-08-11 16:15:07 +0800

    2025/08/11

---
+++ pythonProject/deeplearning/DNN/day1.py
@@ -0,0 +1,74 @@
+# PyTorch
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+# For data preprocess
+import numpy as np
+import csv
+import os
+
+# For plotting
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+myseed = 42069  # set a random seed for reproducibility
+#设置为 True 时，CUDA 的卷积操作将使用确定性算法，确保每次运行结果相同
+torch.backends.cudnn.deterministic = True
+#关闭自动寻找最优卷积算法的功能，因为这个过程本身可能引入随机性
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+#设置PyTorch的CPU随机种子
+torch.manual_seed(myseed)
+# 如果有GPU，设置所有GPU的随机种子
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
+
+
+def get_device():
+    ''' Get device (if GPU is available, use GPU) '''
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
+def plot_learning_curve(loss_record, title=''):
+    ''' Plot learning curve of your DNN (train & dev loss) '''
+    total_steps = len(loss_record['train'])
+    x_1 = range(total_steps)
+    x_2 = x_1[::len(loss_record['train']) // len(loss_record['dev'])]
+    figure(figsize=(6, 4))
+    plt.plot(x_1, loss_record['train'], c='tab:red', label='train')
+    plt.plot(x_2, loss_record['dev'], c='tab:cyan', label='dev')
+    plt.ylim(0.0, 5.)
+    plt.xlabel('Training steps')
+    plt.ylabel('MSE loss')
+    plt.title('Learning curve of {}'.format(title))
+    plt.legend()
+    plt.show()
+
+
+def plot_pred(dv_set, model, device, lim=35., preds=None, targets=None):
+    ''' Plot prediction of your DNN '''
+    if preds is None or targets is None:
+        model.eval()#评估模式切换
+        preds, targets = [], []
+        for x, y in dv_set:
+            x, y = x.to(device), y.to(device)
+            with torch.no_grad():
+                pred = model(x)
+                preds.append(pred.detach().cpu())
+                targets.append(y.detach().cpu())
+        preds = torch.cat(preds, dim=0).numpy()
+        targets = torch.cat(targets, dim=0).numpy()
+
+    figure(figsize=(5, 5))
+    plt.scatter(targets, preds, c='r', alpha=0.5)
+    plt.plot([-0.2, lim], [-0.2, lim], c='b')
+    plt.xlim(-0.2, lim)
+    plt.ylim(-0.2, lim)
+    plt.xlabel('ground truth value')
+    plt.ylabel('predicted value')
+    plt.title('Ground Truth v.s. Prediction')
+    plt.show()
+
+a=get_device()
+print(a)
