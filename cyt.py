affb4b8bb280d4f78182556a02553a437867d1a8
pingshenchen <cyt370746@gmail.com>
2025-08-12 16:09:19 +0800

    2025/08/12

--- cyt.py
+++ cyt.py
@@ -1,28 +1,9 @@
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import torch
-import flask
-from gensim.models import Word2Vec
-import jieba
-sentences = [
-    list(jieba.cut('我喜歡吃蘋果')),
-    list(jieba.cut('蘋果很好吃')),
-    list(jieba.cut('水果是健康的')),
-    list(jieba.cut('梨子也很好吃')),
-    list(jieba.cut('我也喜歡吃柳丁')),
-    list(jieba.cut('蘋果柳丁都是一種水果')),
-    list(jieba.cut('蘋果是一種又香又甜的水果')),
-    list(jieba.cut('梨子跟柳丁也是一種又香又甜的水果')),
-]
+with open(r'/Users/mac/PycharmProjects/python_code/pythonProject/deeplearning/DNN/ml2021spring-hw1/covid.train.csv', 'r') as f:
+    train_data = f.readlines()
+    train_data = [line.split('\n') for line in train_data][1:]  # 分行之后不要第一行
+    train_data = [each[0].split(',') for each in train_data]  # 对于每一行 去掉后面的空格
+    train_data = np.array(train_data)  # 转换成numpy的矩阵
 
-# 訓練詞向量模型
-model = Word2Vec(sentences, window=5, min_count=1, workers=4)
-
-# 獲取所有詞
-vocab = model.wv.index_to_key
-
-# 獲取所有詞向量
-vectors = model.wv[vocab]
-
-print(vectors)
+    train_x = train_data[:, 1:-1]  # x是数据，y是标签 。第一个冒号表示所有行，第二个冒号表示
+    train_y = train_data[:, -1]  # 列。所以x就是第2列到倒数第二列。y就是倒数第一列。
